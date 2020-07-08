a59939555bc59d80f81a1a27411417fcf31b0984
raymondgggg <raymond13gggg@gmail.com>
2020-07-08 09:09:32 -0700

    More work with inheritance

---
+++ Section6/InheritanceChallenge/Main.java
@@ -0,0 +1,7 @@
+// Remember: All classes from Java inherit from java.lang.Object
+public class Main{
+    public static void main(String[] args) {
+        // Showing how the cylinder class inherits from the circle class
+        
+    }
+}
