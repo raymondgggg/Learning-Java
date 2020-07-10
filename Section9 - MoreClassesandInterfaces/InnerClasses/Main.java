6f54f0ab72713c4f3733a0b7381d94052f842719
raymondgggg <raymond13gggg@gmail.com>
2020-07-10 07:34:22 -0700

    inner classes

--- Section9 - MoreClassesandInterfaces/InnerClasses/Main.java
+++ Section9 - MoreClassesandInterfaces/InnerClasses/Main.java
@@ -1,5 +1,32 @@
-public class Main{
+import java.util.Scanner;
+
+
+
+public class Main {
+    private static Scanner scanner = new Scanner(System.in);
+    private static Button btnPrint = new Button("Print");
     public static void main(String[] args) {
+    //     class ClickListener implements Button.OnClickListener{
+    //         public ClickListener(){
+    //             System.out.println("I've been attached");
+    //         }
+            
+    //         @Override 
+    //         public void onClick(String title){
+    //             System.out.println(title + " was clicked");
+    //         }
+    //     }
+
+    //     btnPrint.setOnClickListener(new ClickListener());
+        btnPrint.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(String title){
+                System.out.println(title + " was clicked");
+            }
+        });
+        listen();
+
+
         /*In Java it is actually possible to nest a class within another
         class, there are four types of nested classes: 
             - Static nested classes
@@ -7,14 +34,39 @@ public class Main{
             - Local class (inner class defined inside a scope block)
             - Anonymous class (nested class w/o class name */
 
-        Gearbox mcLaren = new Gearbox(6);
+        // Gearbox mcLaren = new Gearbox(6);
 
-        Gearbox.Gear first = mcLaren.new Gear(1, 12.3);
-        System.out.println(first.driveSpeed(1000));
+        // mcLaren.addGear(1, 5.3); //This way of doing things encapsulates the gear class
+        // mcLaren.addGear(2, 10.6);
+        // mcLaren.addGear(3, 15.9);
 
+        // mcLaren.operateClutch(true);
+        // mcLaren.changeGear(1);
+        // mcLaren.operateClutch(false);
+        // System.out.println(mcLaren.wheelSpeed(1000));
+        // mcLaren.changeGear(2);
+        // System.out.println(mcLaren.wheelSpeed(1000));
+        // mcLaren.operateClutch(true);
+        // mcLaren.changeGear(3);
+        // mcLaren.operateClutch(false);
+        // System.out.println(mcLaren.wheelSpeed(6000));
         //Most inner classes are actuall private 
 
         // Gearbox.Gear second = new Gearbox.Gear(2, 15.4); <--- Leads to error
        // System.out.println(second.driveSpeed(1000));
+    }
+    private static void listen(){
+        boolean quit = false;
+        while (!quit){
+            int choice = scanner.nextInt();
+            scanner.nextLine();
+            switch(choice){
+                case 0:
+                    quit = true;
+                    break;
+                case 1:
+                    btnPrint.onClick();
+            }
+        }
     }
 }
